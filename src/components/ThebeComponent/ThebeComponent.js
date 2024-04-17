2c3c48ceb2dbe242ad569006fbf8e1a24ea4703d
nardoguy14 <nardoarevalo@me.com>
2024-04-16 22:33:18 -0700

    removing use effect

--- src/components/ThebeComponent/ThebeComponent.js
+++ src/components/ThebeComponent/ThebeComponent.js
@@ -8,8 +8,10 @@ import { useState } from 'react';
 import '../../css/dracula.css'
 import '../../css/lesser-dark.css'
 import './ThebeComponent.scss';
+
 function ThebeComponent() {
     const [data, setData] = useState({cells: []});      // To store fetched data
+
 
     if(data.cells.length === 0) {
         fetch('https://raw.githubusercontent.com/nardoguy14/jupyter_notebooks/main/notebooks/iris_notebook.ipynb').then(resp => {
@@ -23,8 +25,10 @@ function ThebeComponent() {
     else {
         const REACT_APP_JUPYTER_SERVER_HOST = process.env.REACT_APP_JUPYTER_SERVER_HOST;
         const REACT_APP_JUPYTER_SERVER_TOKEN = process.env.REACT_APP_JUPYTER_SERVER_TOKEN
+
+        console.log("loadinggg")
         window.thebe.bootstrap({
-            url: `http://${REACT_APP_JUPYTER_SERVER_HOST}:8889?token=${REACT_APP_JUPYTER_SERVER_TOKEN}`,
+            url: `http://${REACT_APP_JUPYTER_SERVER_HOST}:8888/?token=${REACT_APP_JUPYTER_SERVER_TOKEN}`,
             selector: '[data-executable]',
             requestKernel: true,
             predefinedOutput: true,
@@ -36,8 +40,6 @@ function ThebeComponent() {
             },
 
         })
-        console.log('Waited for 2 seconds');
-
         let restartElements  = document.getElementsByClassName('thebe-restart-button');
         let restartAllElements  = document.getElementsByClassName('thebe-restartall-button');
         for(var i = 0; i < restartElements.length; i++) {
