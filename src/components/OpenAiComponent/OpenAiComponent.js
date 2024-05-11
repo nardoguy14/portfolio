4f9904d72f685377632ff7f0789d3669402dd355
nardoguy14 <nardoarevalo@me.com>
2024-05-11 15:26:24 -0700

    Making changes to ui

--- src/components/OpenAiComponent/OpenAiComponent.js
+++ src/components/OpenAiComponent/OpenAiComponent.js
@@ -9,7 +9,7 @@ function OpenAiComponent() {
     const [formValues, setFormValues] = useState({
         url: '',
         embeddings_type: '',
-        max_depth: 5
+        max_depth: 1
     });
 
     const handleInputChange = (event) => {
@@ -41,12 +41,12 @@ function OpenAiComponent() {
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Data Scrape Url</Form.Label>
-                <Form.Control onChange={handleInputChange} name={"url"} value={formValues['url']} type="input" placeholder="Base email" />
+                <Form.Control onChange={handleInputChange} name={"url"} value={formValues['url']} type="input" placeholder="http://example.com" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Embeddings Type</Form.Label>
-                <Form.Control onChange={handleInputChange} name={"embeddings_type"} value={formValues['embeddings_type']} type="input" placeholder="A name for the embeddings being created" />
+                <Form.Control onChange={handleInputChange} name={"embeddings_type"} value={formValues['embeddings_type']} type="input" placeholder="example_embeddings_type" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
