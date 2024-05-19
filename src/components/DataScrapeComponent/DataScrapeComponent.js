5142c6608e67ccbd29667a9fcc9784bd07a73578
nardoguy14 <nardoarevalo@me.com>
2024-05-18 21:19:51 -0700

    adding in status prelim

---
+++ src/components/DataScrapeComponent/DataScrapeComponent.js
@@ -0,0 +1,71 @@
+import {Button, Form} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
+import {useState} from "react";
+
+
+function DataScrapeComponent({setDataScrapeId}) {
+
+    const [formValues, setFormValues] = useState({
+        url: '',
+        embeddings_type: '',
+        max_depth: 1
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormValues({
+            ...formValues,
+            [name]: value
+        });
+    };
+
+    const handleDataScrapeSubmit = (e) => {
+        e.preventDefault()
+        var data = {
+            url: formValues['url'],
+            embeddings_type: formValues['embeddings_type'],
+            max_depth: formValues['max_depth']
+        }
+        console.log(data)
+        axios.post("http://localhost:8009/job/data_scrape", data).then(response =>{
+            console.log(response)
+            setTimeout(()=> {
+                axios.get("http://localhost:8009/job/data_scrape?embeddings_type="+data.embeddings_type).then(response2 => {
+                    console.log(response2)
+                    setDataScrapeId(response2.data[0]['__values__'].id)
+                })
+            }, 3000);
+        })
+    }
+
+
+
+    return (
+    <Container>
+        <h2>Data Scrape & Generate Embeddings</h2>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Data Scrape Url</Form.Label>
+                <Form.Control onChange={handleInputChange} name={"url"} value={formValues['url']} type="input" placeholder="http://example.com" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Embeddings Type</Form.Label>
+                <Form.Control onChange={handleInputChange} name={"embeddings_type"} value={formValues['embeddings_type']} type="input" placeholder="example_embeddings_type" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Depth of Levels</Form.Label>
+                <Form.Control onChange={handleInputChange} name={"max_depth"} type="input" value={formValues['max_depth']} />
+            </Form.Group>
+
+
+            <Button variant="primary" type="submit" onClick={e => handleDataScrapeSubmit(e)}>
+                Submit
+            </Button>
+        </Form>
+    </Container>)
+}
+
+export default DataScrapeComponent
--- src/components/OpenAiComponent/OpenAiComponent.js
+++ src/components/OpenAiComponent/OpenAiComponent.js
@@ -1,66 +1,31 @@
-import {Button, Form} from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import axios from "axios";
 import {useState} from "react";
-
+import DataScrapeComponent from "../DataScrapeComponent/DataScrapeComponent";
+import axios from "axios";
 
 function OpenAiComponent() {
+    let FINISHED_STATUS = "FINISHED SCRAPING"
+    const [dataScrapeId, setDataScrapeId] = useState(null);
+    const [job_status, setDataScrapeStatus] = useState({seen: 0, queue: 0, status: null})
 
-    const [formValues, setFormValues] = useState({
-        url: '',
-        embeddings_type: '',
-        max_depth: 1
-    });
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormValues({
-            ...formValues,
-            [name]: value
-        });
-    };
-
-    const handleDataScrapeSubmit = (e) => {
-        e.preventDefault()
-        var data = {
-            url: formValues['url'],
-            embeddings_type: formValues['embeddings_type'],
-            max_depth: formValues['max_depth']
+    if(dataScrapeId != null){
+        if(job_status.status !== FINISHED_STATUS) {
+            axios.get(`http://localhost:8009/job/data_scrape/${dataScrapeId}`).then(response2 => {
+                console.log(response2)
+                const values = response2.data['__values__']
+                setDataScrapeStatus({seen: values.sites_seen, queue: values.queue_size, status: values.status})
+            })
         }
-        console.log(data)
-        axios.post("http://localhost:8009/job/data_scrape", data).then(response =>{
-            console.log(response)
-
-        })
     }
 
-
-
     return (
-    <Container>
-        <h2>Data Scrape & Generate Embeddings</h2>
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Data Scrape Url</Form.Label>
-                <Form.Control onChange={handleInputChange} name={"url"} value={formValues['url']} type="input" placeholder="http://example.com" />
-            </Form.Group>
+        <div>
+        <DataScrapeComponent setDataScrapeId={setDataScrapeId} />
+            {job_status.status && <div>
+                Queue size: {job_status.queue} / Seen size: {job_status.seen}
+            </div>}
+        </div>
+    )
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Embeddings Type</Form.Label>
-                <Form.Control onChange={handleInputChange} name={"embeddings_type"} value={formValues['embeddings_type']} type="input" placeholder="example_embeddings_type" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Depth of Levels</Form.Label>
-                <Form.Control onChange={handleInputChange} name={"max_depth"} type="input" value={formValues['max_depth']} disabled/>
-            </Form.Group>
-
-
-            <Button variant="primary" type="submit" onClick={e => handleDataScrapeSubmit(e)}>
-                Submit
-            </Button>
-        </Form>
-    </Container>)
 }
 
-export default OpenAiComponent
+export default OpenAiComponent;
